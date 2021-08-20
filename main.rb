b2247bd85b56229947c2e04efe9ed96af8b157eb
Rafael Briones <rbrionesdev@outlook.com>
2021-08-20 10:56:10 -0600

    added comments

---
+++ main.rb
@@ -0,0 +1,9 @@
+puts "hello"
+
+# add: stages changes gets them to be commited
+# git add . to stage all changes
+# get add filename to stage individual changes
+
+# git commit -m 'describe what you did since last commit'
+
+## create a function that adds two numbers
